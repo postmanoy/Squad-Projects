40f1d5ef94c663a274a6506603498b117104c4bc
clarkp <clark_panares@trendmicro.com>
2020-01-29 11:02:52 +0800

    v1

--- PHDSaaS/index-old.php
+++ PHDSaaS/index-old.php
@@ -36,7 +36,7 @@
               // Add hash (#) to URL when done scrolling (default click behavior)
               window.location.hash = hash;
             });
-          }  // End ifssss
+          }  // End if
         });
       });
   </script>
